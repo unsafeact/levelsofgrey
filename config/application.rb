f25648a385a708e5060fee8ee81316431c858700
unsafeact <un@safe.act>
2018-12-17 14:11:43 +0100

    fixing custom webfonts

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Levelsofgrey
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+# for custom webfonts in assets/fonts/
+config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
